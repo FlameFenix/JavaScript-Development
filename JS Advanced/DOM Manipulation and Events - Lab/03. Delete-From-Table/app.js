78fbc051623d6fb37149add1bc8da76ad4d3d18b
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-06-05 00:53:01 +0300

    Lab

--- JS Advanced/DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
+++ JS Advanced/DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
@@ -2,17 +2,19 @@ function deleteByEmail() {
     let tableElements = document.querySelectorAll('td:nth-child(2)');
     let deleteByEmailInputElement = document.getElementsByName('email');
     let divResultElement = document.getElementById('result');
+    let isFound = false;
 
     for (const element of tableElements) {
         if (element.textContent == deleteByEmailInputElement[0].value) {
-            console.log(element.textContent);
             element.parentElement.remove(element);
             divResultElement.textContent = 'Deleted';
-        } else {
-            divResultElement.textContent = 'Not found.';
+            isFound = true;
         }
     }
 
+    if(!isFound) {
+        divResultElement.textContent = 'Not found.';
+    }
 
     deleteByEmailInputElement[0].value = ''
 }
