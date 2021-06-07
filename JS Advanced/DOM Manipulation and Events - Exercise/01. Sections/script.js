deb4debf47912638b8e1e35786f6cda7a8818c6b
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-06-07 22:57:17 +0300

    Exercises

---
+++ JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,23 @@
+function create(words) {
+   let divElement = document.getElementById('content');
+
+   for (const element of words) {
+      let newDivElement = document.createElement('div');
+      let newParagraphElement = document.createElement('p');
+
+      newParagraphElement.textContent = element;
+      newParagraphElement.style.display = 'none';
+
+      newDivElement.appendChild(newParagraphElement);
+      divElement.appendChild(newDivElement);
+
+      divElement.addEventListener('click', showParagraph)
+   }
+
+
+   function showParagraph(e) {
+      e.target.children[0].style.display = '';
+      console.log(e.target.children[0]);
+      console.log(e);
+   }
+}
