45d6862d537087062acedd5db1da57f3e15c7603
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-06-12 00:44:08 +0300

    In progress

--- JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -3,11 +3,17 @@ function lockedProfile() {
     let profileElements = Array.from(document.querySelectorAll('.profile'));
 
     for (const profile of profileElements) {
-        let inputElements = Array.from(profile.querySelectorAll('input:checked'))
+        let inputElements = Array.from(profile.querySelectorAll('input'))
             .filter(x => x.type == 'radio');
         for (const radioBtns of inputElements) {
             {
-                
+                radioBtns.addEventListener('change',() => {
+                    if(radioBtns.value == 'unlock') {
+                        
+                    } else {
+
+                    }
+                })
             }
         }
     }
