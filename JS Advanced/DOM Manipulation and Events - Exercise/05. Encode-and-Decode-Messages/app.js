24ba5691ad337ff48df142cae0e2c542d557895f
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-06-13 18:39:44 +0300

    Exercises

--- JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -2,33 +2,33 @@ function encodeAndDecodeMessages() {
     let buttonsElements = Array.from(document.querySelectorAll('button'));
     buttonsElements[0].addEventListener('click', () => {
 
-            let parrentTag = buttonsElements[0].parentElement;
-            let textArea = parrentTag.querySelector('textarea');
-            let codedMessage = '';
+        let parrentTag = buttonsElements[0].parentElement;
+        let textArea = parrentTag.querySelector('textarea');
+        let codedMessage = '';
 
-            let secondButton = buttonsElements[1];
-            let textAreaTwo = secondButton.parentElement.querySelector('textarea');
+        let secondButton = buttonsElements[1];
+        let textAreaTwo = secondButton.parentElement.querySelector('textarea');
 
-            for (let i = 0; i < textArea.value.length; i++) {
-                let number = textArea.value.charCodeAt(i);
-                codedMessage += String.fromCharCode(number + 1);
-            }
+        for (let i = 0; i < textArea.value.length; i++) {
+            let number = textArea.value.charCodeAt(i);
+            codedMessage += String.fromCharCode(number + 1);
+        }
 
-            textArea.value = '';
-            textAreaTwo.value = codedMessage;
+        textArea.value = '';
+        textAreaTwo.value = codedMessage;
 
-        })
+    })
 
-        buttonsElements[1].addEventListener('click', () => {
+    buttonsElements[1].addEventListener('click', () => {
 
-            let codedMessage = '';
-            let secondButton = buttonsElements[1];
-            let textAreaTwo = secondButton.parentElement.querySelector('textarea');
+        let codedMessage = '';
+        let secondButton = buttonsElements[1];
+        let textAreaTwo = secondButton.parentElement.querySelector('textarea');
 
-            for (let i = 0; i < textAreaTwo.value.length; i++) {
-                let number = textAreaTwo.value.charCodeAt(i);
-                codedMessage += String.fromCharCode(number - 1);
-            }
-            textAreaTwo.value = codedMessage;
-        })
+        for (let i = 0; i < textAreaTwo.value.length; i++) {
+            let number = textAreaTwo.value.charCodeAt(i);
+            codedMessage += String.fromCharCode(number - 1);
+        }
+        textAreaTwo.value = codedMessage;
+    })
 }
