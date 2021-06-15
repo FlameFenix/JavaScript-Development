39cadd4dc963bf1690613204a71455abb98ff462
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-06-16 01:34:45 +0300

    Lab

--- JS Advanced/Classes and Attributes - Lab/getPersons.js
+++ JS Advanced/Classes and Attributes - Lab/getPersons.js
@@ -7,18 +7,18 @@ function getPersons() {
             this.age = age;
             this.email = email;
         }
-    
+
         toString() {
             return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
         }
     }
-    
+
     let personOne = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
     let personTwo = new Person('SoftUni');
     let personThree = new Person('Stephan', 'Johnson', 25);
     let personFour = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
 
-    let arr = [personOne,personTwo,personThree,personFour];
+    let arr = [personOne, personTwo, personThree, personFour];
 
     return arr;
 }
